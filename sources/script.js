58bdebd1b7fc796fca52a964b8ab587c2a3df085
jamieranick <jamieranick@gmail.com>
2026-05-03 14:41:55 +0200

    updated api for weather search data

--- sources/script.js
+++ sources/script.js
@@ -1,9 +1,39 @@
+function updateWeatherInfo(response) {
+  let refreshTemp = document.querySelector("#currentTemp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#cityName");
+  let countryElement = document.querySelector("#countryName");
+  let conditionsElement = document.querySelector("#conditions");
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  let feelsTemp = response.data.temperature.feels_like;
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+
+  refreshTemp.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
+  conditionsElement.innerHTML = response.data.condition.description;
+  feelsLikeElement.innerHTML = Math.round(feelsTemp);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(wind);
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "a7ftc20e6332a5fbb4e6e025bb1oaa93";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function newCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#cityInput");
-  let cityElement = document.querySelector("#cityName");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", newCity);
+
+searchCity("Maarssen");
